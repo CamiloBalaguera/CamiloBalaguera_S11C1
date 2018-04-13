8bfc8106c85dc403d370d2649a9aeb96cd3c4df4
Camilo Balaguera <ca.balaguera@uniandes.edu.co>
2018-04-13 18:11:34 -0500

    Ahora el programa imprime al final correctamente el tiempo en el que el numero de infectados es máximo, además el programa ahora grafica ambos casos.

--- CamiloBalaguera_S11C1.cpp
+++ CamiloBalaguera_S11C1.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
 using namespace std;
 
-const int n = 100;
+const int n = 1000;
 int N = 771;
 double ti = 0.0;
-double tf = 100.0;
+double tf = 50.0;
 double S[n];
 double I[n];
 double t[n];
 double h = (tf-ti)/n;
-int diferencial(double Beta, double Gamma); 
+int diferencial(double Beta, double Gamma, int Caso); 
+int diferencial2(double Beta, double Gamma, int Caso); 
+int busqueda(double* lista);
 
 int main()
 {
-	diferencial(0.0022, 0.45);
-	cout << "Caso 1" << "," << "tiempo I_max(dias):" << t[n-1] << endl;
-	diferencial(0.001, 0.2);
-	cout << "Caso 2" << "," << "tiempo I_max(dias):" << t[n-1] << endl;
+	diferencial(0.0022, 0.45, 1);
+	diferencial(0.001, 0.2, 2);
+	diferencial2(0.0022, 0.45, 1);
+	diferencial2(0.001, 0.2, 2);
 	return 0;
 }
 
-int diferencial(double Beta, double Gamma)
+int diferencial(double Beta, double Gamma, int Caso )
 {
 	S[0] = 770;
 	I[0] = 1;
+	double maximo = 0.0;
+	double infect = 0.0;
 	for (int i = 1; i < n; i++)
 	{
-		if (I[i-1] <= N-1)
+		if (I[i-1] <= N) 
 		{ 
 			t[i] = i*h;
 			S[i] = -h*Beta*I[i-1]*S[i-1] + S[i-1];
 			I[i] = h*((Beta*I[i-1]*S[i-1]) - (Gamma*I[i-1])) + I[i-1];
 			cout << t[i] << " " << S[i] << " " << I[i] << endl;
+			if (I[i] > infect)
+			{
+				infect = I[i];
+				maximo = t[i];
+			}
 		}
 		else
 		{
@@ -40,4 +49,33 @@ int diferencial(double Beta, double Gamma)
 	}
 	return 1;
 }
+
+int diferencial2(double Beta, double Gamma, int Caso )
+{
+	S[0] = 770;
+	I[0] = 1;
+	double maximo = 0.0;
+	double infect = 0.0;
+	for (int i = 1; i < n; i++)
+	{
+		if (I[i-1] <= N) 
+		{ 
+			t[i] = i*h;
+			S[i] = -h*Beta*I[i-1]*S[i-1] + S[i-1];
+			I[i] = h*((Beta*I[i-1]*S[i-1]) - (Gamma*I[i-1])) + I[i-1];
+			if (I[i] > infect)
+			{
+				infect = I[i];
+				maximo = t[i];
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	cout << "Caso " << Caso << "," << "tiempo I_max(dias):" << maximo << endl;
+	return 1;
+}
+
 
--- CamiloBalaguera_S11C1_PLOTS.py
+++ CamiloBalaguera_S11C1_PLOTS.py
@@ -1,11 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.genfromtxt("Datos1.txt", usecols = (0))
-y1 = np.genfromtxt("Datos1.txt", usecols = (1))
-y2 = np.genfromtxt("Datos2.txt", usecols = (1))
+t = np.genfromtxt("datos.txt", usecols = (0))
+y1 = np.genfromtxt("datos.txt", usecols = (1))
+y2 = np.genfromtxt("datos.txt", usecols = (2))
+
 
 plt.figure()
-plt.plot(t, y1, label = "Caso 1")
-plt.plot(t, y2, label = "Caso 2")
+plt.plot(np.transpose(t)[:998], np.transpose(y1)[:998], label = "Caso 1, Poblacion sana")
+plt.plot(np.transpose(t)[:998], np.transpose(y2)[:998], label = "Caso 1, Poblacion infectada", c = "r")
+plt.plot(np.transpose(t)[1000:-3], np.transpose(y1)[1000:-3], label = "Caso 2, Poblacion sana", c = "k")
+plt.plot(np.transpose(t)[1000:-3], np.transpose(y2)[1000:-3], label = "Caso 2, Poblacion infectada", c = "g")
 plt.legend(loc=0)
+plt.savefig("SIR.pdf")
+
